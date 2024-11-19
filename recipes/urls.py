9f0b7c6cd7a06960c2599cbf6bcdf6df95247a58
Fernando Alves Carreira <fe.alves_carreira@hotmail.com>
2024-11-19 20:32:59 -0300

    Removido algumas páginas

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,8 +5,6 @@ from recipes.views import contato, home, sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contato/', contato),
-    path('sobre/', sobre),
     path('', home), # HOME
 ]
 
--- recipes/views.py
+++ recipes/views.py
@@ -6,9 +6,3 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'recipes/home.html')
     # return HttpResponse('Pagina inicial 1')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
